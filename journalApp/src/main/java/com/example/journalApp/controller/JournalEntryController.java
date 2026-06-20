26a1a322f11c36bb782949a0e3f28da61c1ab838
albashzia <ahmedalbashzia@gmail.com>
2026-06-20 10:07:15 +0500

    Adding RestController annotation to JournalEntryController class and defining a method to return all the Journal entries in the form of an array list

--- journalApp/src/main/java/com/example/journalApp/controller/JournalEntryController.java
+++ journalApp/src/main/java/com/example/journalApp/controller/JournalEntryController.java
@@ -1,4 +1,20 @@
 package com.example.journalApp.controller;
 
+
+import com.example.journalApp.entity.JournalEntry;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
 public class JournalEntryController {
+
+    private Map<Long,JournalEntry> journalEntries = new HashMap<>();
+
+    public List<JournalEntry> getAll(){
+        return new ArrayList<>(journalEntries.values());
+    }
 }
