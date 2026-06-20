083e9d8b5e729cef950a05f10766592958c934fe
albashzia <ahmedalbashzia@gmail.com>
2026-06-20 10:16:51 +0500

    Adding an end path to RequestMapping annotation in JournalEntryController

--- journalApp/src/main/java/com/example/journalApp/controller/JournalEntryController.java
+++ journalApp/src/main/java/com/example/journalApp/controller/JournalEntryController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping
+@RequestMapping("/journal")
 public class JournalEntryController {
 
     private Map<Long,JournalEntry> journalEntries = new HashMap<>();
