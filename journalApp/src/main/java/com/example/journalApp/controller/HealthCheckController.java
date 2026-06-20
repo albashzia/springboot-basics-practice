0f5f76660fe28cd8592f6d80e36a020d56ad6a04
albashzia <ahmedalbashzia@gmail.com>
2026-06-20 09:56:14 +0500

    Modified the mapping path, removing an extra space

--- journalApp/src/main/java/com/example/journalApp/controller/HealthCheckController.java
+++ journalApp/src/main/java/com/example/journalApp/controller/HealthCheckController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthCheckController {
 
-    @GetMapping("/health-check ")
+    @GetMapping("/health-check")
     public String healthCheck(){
         return "Ok";
     }
