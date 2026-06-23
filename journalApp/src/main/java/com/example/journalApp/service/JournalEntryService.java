3461c263f136d6fae732de553e541d5d825a3550
albashzia <ahmedalbashzia@gmail.com>
2026-06-23 21:42:05 +0500

    Injecting JournalEntryRepository into JournalEntryService class

--- journalApp/src/main/java/com/example/journalApp/service/JournalEntryService.java
+++ journalApp/src/main/java/com/example/journalApp/service/JournalEntryService.java
@@ -1,4 +1,10 @@
 package com.example.journalApp.service;
 
+import com.example.journalApp.repository.JournalEntryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class JournalEntryService {
+
+    @Autowired
+    private JournalEntryRepository journalEntryRepository;
 }
