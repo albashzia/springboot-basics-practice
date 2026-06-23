13b7195956af6e9ab2293c4457473189217a4179
albashzia <ahmedalbashzia@gmail.com>
2026-06-23 21:38:35 +0500

    Adding @Id annotation to id attribute of JournalEntry class to define the unique key for MongoDB collection

--- journalApp/src/main/java/com/example/journalApp/entity/JournalEntry.java
+++ journalApp/src/main/java/com/example/journalApp/entity/JournalEntry.java
@@ -1,11 +1,14 @@
 package com.example.journalApp.entity;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
 public class JournalEntry {
 
+    @Id
     private String id;
+
     private String title;
     private String content;
 
