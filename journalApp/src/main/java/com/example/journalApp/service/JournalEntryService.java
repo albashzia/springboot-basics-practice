3b15d6e2ca2232826a93bad292da20d5b8269207
albashzia <ahmedalbashzia@gmail.com>
2026-06-23 22:11:31 +0500

    Adding @Component annotation to JournalEntryService class

--- journalApp/src/main/java/com/example/journalApp/service/JournalEntryService.java
+++ journalApp/src/main/java/com/example/journalApp/service/JournalEntryService.java
@@ -3,7 +3,9 @@ package com.example.journalApp.service;
 import com.example.journalApp.entity.JournalEntry;
 import com.example.journalApp.repository.JournalEntryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class JournalEntryService {
 
     @Autowired
