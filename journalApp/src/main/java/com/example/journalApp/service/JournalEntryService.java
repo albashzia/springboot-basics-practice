9fcebea2f464ea4e9f3d8e500319aa53cf1b1308
albashzia <ahmedalbashzia@gmail.com>
2026-06-23 21:50:48 +0500

    Creating a saveEntry method in JournalEntryService class which takes a JournalEntry as an attribute and saves it to the database using JournalEntryRepository

--- journalApp/src/main/java/com/example/journalApp/service/JournalEntryService.java
+++ journalApp/src/main/java/com/example/journalApp/service/JournalEntryService.java
@@ -1,5 +1,6 @@
 package com.example.journalApp.service;
 
+import com.example.journalApp.entity.JournalEntry;
 import com.example.journalApp.repository.JournalEntryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -7,4 +8,8 @@ public class JournalEntryService {
 
     @Autowired
     private JournalEntryRepository journalEntryRepository;
+
+    public void saveEntry(JournalEntry journalEntry){
+        journalEntryRepository.save(journalEntry);
+    }
 }
