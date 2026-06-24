cfd439e7781bd2a54e379bcdfefa51928cd28904
albashzia <ahmedalbashzia@gmail.com>
2026-06-24 11:29:12 +0500

    Changing the data type of date attribute in JournalEntry from Date to LocalDateTime

--- journalApp/src/main/java/com/example/journalApp/entity/JournalEntry.java
+++ journalApp/src/main/java/com/example/journalApp/entity/JournalEntry.java
@@ -4,6 +4,7 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Document(collation = "journal_entries")
@@ -14,7 +15,7 @@ public class JournalEntry {
 
     private String title;
     private String content;
-    private Date date;
+    private LocalDateTime date;
 
 
     public ObjectId getId() {
@@ -41,11 +42,11 @@ public class JournalEntry {
         this.content = content;
     }
 
-    public Date getDate() {
+    public LocalDateTime getDate() {
         return date;
     }
 
-    public void setDate(Date date) {
+    public void setDate(LocalDateTime date) {
         this.date = date;
     }
 }
