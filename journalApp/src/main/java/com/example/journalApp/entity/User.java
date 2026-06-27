c30ac403e9271906a1736997df0a6a28d4a2ca2a
albashzia <ahmedalbashzia@gmail.com>
2026-06-27 20:42:08 +0500

    Adding a new class named User in entity package and defining its attributes along with some annotations

---
+++ journalApp/src/main/java/com/example/journalApp/entity/User.java
@@ -0,0 +1,21 @@
+package com.example.journalApp.entity;
+
+import lombok.Data;
+import lombok.NonNull;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
+@Data
+public class User {
+
+    @Id
+    private ObjectId id;
+    @Indexed(unique = true)
+    @NonNull
+    private String userName;
+    @NonNull
+    private String password;
+}
