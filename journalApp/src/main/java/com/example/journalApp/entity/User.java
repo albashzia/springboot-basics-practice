49eee7b8ade79295c972ce2f7aeef746c5cd414b
albashzia <ahmedalbashzia@gmail.com>
2026-06-27 21:44:17 +0500

    Adding a List attribute to User class with @DBRef annotation to hold the reference value of some collection or record

--- journalApp/src/main/java/com/example/journalApp/entity/User.java
+++ journalApp/src/main/java/com/example/journalApp/entity/User.java
@@ -5,7 +5,11 @@ import lombok.NonNull;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Document(collection = "users")
 @Data
@@ -18,4 +22,7 @@ public class User {
     private String userName;
     @NonNull
     private String password;
+    @DBRef
+    private List<JournalEntry> journalEntries = new ArrayList<>();
+
 }
