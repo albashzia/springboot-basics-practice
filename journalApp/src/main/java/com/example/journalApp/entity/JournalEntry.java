853cf416fde8aca50782636ffae1b43b8424100c
albashzia <ahmedalbashzia@gmail.com>
2026-06-27 20:43:33 +0500

    Adding NonNull annotation to title attribute in JournalEntry class using lombok

--- journalApp/src/main/java/com/example/journalApp/entity/JournalEntry.java
+++ journalApp/src/main/java/com/example/journalApp/entity/JournalEntry.java
@@ -1,12 +1,12 @@
 package com.example.journalApp.entity;
 
 import lombok.Data;
+import lombok.NonNull;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 
 @Document(collection = "journal_entries")
 @Data
@@ -14,10 +14,9 @@ public class JournalEntry {
 
     @Id
     private ObjectId id;
-
+    @NonNull
     private String title;
     private String content;
     private LocalDateTime date;
-
 
 }
