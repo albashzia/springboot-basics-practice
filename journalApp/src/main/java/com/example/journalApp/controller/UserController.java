2110f5132eb9ef0d508c8d1934d533f1db51b7d4
albashzia <ahmedalbashzia@gmail.com>
2026-06-28 23:24:41 +0500

    Injecting UserService as a dependency to UserController and adding a method to return all users with GET call from the API

--- journalApp/src/main/java/com/example/journalApp/controller/UserController.java
+++ journalApp/src/main/java/com/example/journalApp/controller/UserController.java
@@ -1,10 +1,24 @@
  package com.example.journalApp.controller;
 
 
+ import com.example.journalApp.entity.User;
+ import com.example.journalApp.service.UserService;
+ import org.springframework.beans.factory.annotation.Autowired;
  import org.springframework.web.bind.annotation.*;
+
+ import java.util.List;
 
  @RestController
 @RequestMapping("/user")
 public class UserController {
+
+  @Autowired
+  private UserService userService;
+
+
+  @GetMapping
+  public List<User> getAllUsers(){
+   return userService.getAll();
+  }
 
 }
