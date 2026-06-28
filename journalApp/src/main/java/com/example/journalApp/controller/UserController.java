4adfe00f6c57475183984e2238d20a58a04577a3
albashzia <ahmedalbashzia@gmail.com>
2026-06-28 23:21:35 +0500

    Removing unnecessary imports from UserController class

--- journalApp/src/main/java/com/example/journalApp/controller/UserController.java
+++ journalApp/src/main/java/com/example/journalApp/controller/UserController.java
@@ -1,17 +1,7 @@
  package com.example.journalApp.controller;
 
 
- import com.example.journalApp.entity.JournalEntry;
- import com.example.journalApp.service.JournalEntryService;
- import org.bson.types.ObjectId;
- import org.springframework.beans.factory.annotation.Autowired;
- import org.springframework.http.HttpStatus;
- import org.springframework.http.ResponseEntity;
  import org.springframework.web.bind.annotation.*;
-
- import java.time.LocalDateTime;
- import java.util.List;
- import java.util.Optional;
 
  @RestController
 @RequestMapping("/user")
