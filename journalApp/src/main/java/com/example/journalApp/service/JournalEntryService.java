890ee18a2d450c4cd57b10e85ad1141c5fde5841
albashzia <ahmedalbashzia@gmail.com>
2026-07-03 16:28:12 +0500

    Changing the saveEntry method so that it saves the entry to user record instead of a separate entries collection

--- journalApp/src/main/java/com/example/journalApp/service/JournalEntryService.java
+++ journalApp/src/main/java/com/example/journalApp/service/JournalEntryService.java
@@ -1,11 +1,13 @@
 package com.example.journalApp.service;
 
 import com.example.journalApp.entity.JournalEntry;
+import com.example.journalApp.entity.User;
 import com.example.journalApp.repository.JournalEntryRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,9 +16,15 @@ public class JournalEntryService {
 
     @Autowired
     private JournalEntryRepository journalEntryRepository;
+    @Autowired
+    private UserService userService;
 
-    public void saveEntry(JournalEntry journalEntry){
-        journalEntryRepository.save(journalEntry);
+    public void saveEntry(JournalEntry journalEntry, String userName){
+        User user = userService.findByUserName(userName);
+        journalEntry.setDate(LocalDateTime.now());
+        JournalEntry saved = journalEntryRepository.save(journalEntry);
+        user.getJournalEntries().add(saved);
+        userService.saveEntry(user);
     }
 
     public List<JournalEntry> getAll(){
