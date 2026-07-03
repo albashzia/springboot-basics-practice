d197849677b433cbb1fb0f269b02b55823b52199
albashzia <ahmedalbashzia@gmail.com>
2026-07-03 16:54:50 +0500

    Changing deleteById method to accept a userName string, find user, delete the record if it matched the object id and updating the id

--- journalApp/src/main/java/com/example/journalApp/service/JournalEntryService.java
+++ journalApp/src/main/java/com/example/journalApp/service/JournalEntryService.java
@@ -35,7 +35,10 @@ public class JournalEntryService {
         return journalEntryRepository.findById(id);
     }
 
-    public void deleteById(ObjectId id){
+    public void deleteById(ObjectId id, String userName){
+        User user = userService.findByUserName(userName);
+        user.getJournalEntries().removeIf(x-> x.getId().equals(id));
+        userService.saveEntry(user);
         journalEntryRepository.deleteById(id);
     }
 }
